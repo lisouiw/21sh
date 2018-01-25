ce645911a37b0dc7b32aac464d57aa99ee62f23a
Lisa TRAN <ltran@e2r6p14.42.fr>
2018-01-25 17:58:07 +0100

    parsing quote

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -3,6 +3,24 @@
 //quote laisse en raw
 //dquote interprete certaine variable
 
+
+int     parsing_quote(char *s)
+{
+    int     i;
+    int     a;
+
+    i = 0; 
+    a = -1;     
+    while (s[++a])
+    {
+        if (s[a] == '"' && i != 1)
+            i = (i == 0) ? 2 : 0;
+        else if (s[a] == 39 && i != 2)
+            i = (i == 0) ? 1 : 0;
+    }
+    return (i);
+}
+
 int     parsing(t_edit *ed, t_froz **fz)
 {
     char *nw;
@@ -22,23 +40,19 @@ int     parsing(t_edit *ed, t_froz **fz)
     }
     while (ed->rpz[0] == 0)
         ed = ed->next;
-    if (parsing_quote)
-        exit(0);
-    if (ed->c[0] == 39)
-        (*fz)->mode[3] = 1;
-    else if (ed->c[0] == 34)
-        (*fz)->mode[3] = 2;
-    else if (ed->c[0] == '|')
-        (*fz)->mode[3] = 3;
-    else if (ed->c[0] == '&')
-        (*fz)->mode[3] = 4;
-    else if (ed->c[0] == 'o')
-        (*fz)->mode[3] = 5;
-    else if (ed->c[0] == '>') 
-        (*fz)->mode[3] = 6;
-    else
+    if (((*fz)->mode[3] = parsing_quote((*fz)->cmd)))
+        ;
+    // else if (ed->c[0] == '|')
+    //     (*fz)->mode[3] = 3;
+    // else if (ed->c[0] == '&')
+    //     (*fz)->mode[3] = 4;
+    // else if (ed->c[0] == 'o')
+    //     (*fz)->mode[3] = 5;
+    // else if (ed->c[0] == '>') 
+    //     (*fz)->mode[3] = 6;
+    else if ((*fz)->mode[3] == 0)
     {
-        (*fz)->mode[3] = 0;
+        // (*fz)->mode[3] = 0;
         return(1);
     }
     return(0);
